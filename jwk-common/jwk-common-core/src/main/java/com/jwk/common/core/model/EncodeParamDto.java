532fe92deead693401ace3bba553b6965fadf165
jiwk <jiwk@yunjiacloud.com>
2021-12-27 11:30:03 +0800

    add skywalking-agent & jasypt配置文件加密

---
+++ jwk-common/jwk-common-core/src/main/java/com/jwk/common/core/model/EncodeParamDto.java
@@ -0,0 +1,12 @@
+package com.jwk.common.core.model;
+
+import lombok.Data;
+
+@Data
+public class EncodeParamDto {
+
+  private String key;
+
+  private String value;
+
+}
---
+++ jwk-common/jwk-common-core/src/main/java/com/jwk/common/core/utils/EncryptConfigUtil.java
@@ -0,0 +1,53 @@
+package com.jwk.common.core.utils;
+
+
+import com.jwk.common.core.model.EncodeParamDto;
+import java.util.ArrayList;
+import java.util.List;
+import org.jasypt.util.text.BasicTextEncryptor;
+
+/**
+ * jasypt-spring-boot-starter 生成密文的工具代码
+ */
+public class EncryptConfigUtil {
+
+    /**
+     * 配置文件加密
+     * @param salt
+     * @param params
+     * @return
+     */
+    private static List<EncodeParamDto> encodeParamList(String salt, List<EncodeParamDto> params) {
+        List<EncodeParamDto> result = new ArrayList<>();
+        for (EncodeParamDto param : params) {
+            param.setValue(encryptParam(salt,param.getValue()));
+            result.add(param);
+        }
+        return result;
+    }
+
+    /**
+     * 配置文件加密
+     * @param salt
+     * @param param
+     * @return
+     */
+    private static String encryptParam(String salt, String param) {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword(salt);
+        return textEncryptor.encrypt(param);
+    }
+
+    /**
+     * 配置文件解密
+     * @param salt
+     * @param param
+     * @return
+     */
+    private static String decryptParam(String salt, String param) {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword(salt);
+        return textEncryptor.decrypt(param);
+    }
+
+}
